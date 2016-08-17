987a24c18347ecccdc601230d708c630be23adf1
Sunxb <sunxb@boqii.com>
2016-08-17 15:57:17 +0800

    1.侧滑 2. 右上角按钮的动画

---
+++ Yantaiforum/Modules/HomePage/Views/WeatherView.h
@@ -0,0 +1,17 @@
+//
+//  WeatherView.h
+//  Yantaiforum
+//
+//  Created by sunxb on 16/8/17.
+//  Copyright © 2016年 sunxb. All rights reserved.
+//
+
+#import "YTBaseView.h"
+
+@interface WeatherView : YTBaseView
+
+- (void)weatherViewShow;
+
+- (void)wearherViewHidden;
+
+@end
